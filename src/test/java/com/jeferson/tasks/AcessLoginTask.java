ab361b019d16dd38f98f372716c7cd6d4028e1d7
jefynhu11 <jefyx1@hotmail.com>
2021-12-28 15:23:08 -0300

    Adicionei webdriver e classe AcessLoginPage

--- src/test/java/com/jeferson/tasks/AcessLoginTask.java
+++ src/test/java/com/jeferson/tasks/AcessLoginTask.java
@@ -1,20 +1,29 @@
 package com.jeferson.tasks;
 
+import com.jeferson.pages.AcessLoginPage;
 import com.jeferson.runner.RunCucumberTest;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AcessLoginTask extends RunCucumberTest {
+
+    WebDriver driver = new ChromeDriver();
+    AcessLoginPage acessLoginPage = new AcessLoginPage(driver);
 
     @Dado("^que estou no site da loja$")
     public void que_estou_no_site_da_loja() {
         System.out.println("a");
+        acessLoginPage.acessWindowLogin();
+//        driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
     }
 
     @Dado("^eu preencho para criar meu email$")
     public void eu_preencho_para_criar_meu_email() {
         System.out.println("b");
+        acessLoginPage.createEmailField("teste@gmail.com");
     }
 
     @Dado("^clico em botao para inicia criar conta$")
