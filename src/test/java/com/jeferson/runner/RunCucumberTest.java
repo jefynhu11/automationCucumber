320ba6b652f6cbe23f7850cfd044fad8a641fc5f
jefynhu11 <jefyx1@hotmail.com>
2021-12-30 16:53:48 -0300

    Apaguei imports e adicionei plugin para comentario

--- src/test/java/com/jeferson/runner/RunCucumberTest.java
+++ src/test/java/com/jeferson/runner/RunCucumberTest.java
@@ -3,15 +3,12 @@ package com.jeferson.runner;
 import com.jeferson.framework.support.BaseTest;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {},
+//        plugin = {"pretty"},
         features = {"src/test/resources/features"},
         tags = {"@cadastrar-login"},
         glue = {"com/jeferson/tasks"}
