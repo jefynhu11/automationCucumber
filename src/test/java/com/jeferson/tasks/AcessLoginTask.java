f32b1213f75c1ac1731f5b25572e7c8dc1fd3aa3
jefynhu11 <jefyx1@hotmail.com>
2022-01-04 12:44:20 -0300

    Modifiquei e apaguei anotacoes BDD

--- src/test/java/com/jeferson/tasks/AcessLoginTask.java
+++ src/test/java/com/jeferson/tasks/AcessLoginTask.java
@@ -1,17 +1,21 @@
 package com.jeferson.tasks;
 
 import com.jeferson.framework.support.BaseTest;
+import com.jeferson.framework.support.Fakers;
 import com.jeferson.pages.AcessLoginPage;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 
+import java.io.IOException;
+
 public class AcessLoginTask extends BaseTest {
 
     AcessLoginPage acessLoginPage = new AcessLoginPage(driver);
+    Fakers fakers = new Fakers();
 
     @Dado("^que estou no site da loja$")
-    public void que_estou_no_site_da_loja() {
+    public void que_estou_no_site_da_loja() throws IOException {
         System.out.println("a");
         acessLoginPage.acessWindowLogin();
     }
@@ -19,23 +23,13 @@ public class AcessLoginTask extends BaseTest {
     @Dado("^eu preencho para criar meu email$")
     public void eu_preencho_para_criar_meu_email() {
         System.out.println("b");
-        acessLoginPage.createEmailField("testes1@gmail.com");
+        acessLoginPage.createEmailField(fakers.getEmail());
     }
 
     @Dado("^clico em botao para inicia criar conta$")
     public void clico_em_botao_para_inicia_criar_conta() {
         System.out.println("c");
         acessLoginPage.createAnAccountButton();
-    }
-
-    @Quando("^clico em botao para registrar da conta$")
-    public void clico_em_botao_para_registrar_da_conta() {
-        System.out.println("e");
-    }
-
-    @Então("^verificado se cadastrar com sucesso$")
-    public void verificado_se_cadastrar_com_sucesso() {
-        System.out.println("f");
     }
 
 }
